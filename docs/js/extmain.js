255a37a6928a18d080c988944b94362073233161
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-08-23 19:57:21 +0900

    commit

--- docs/js/extmain.js
+++ docs/js/extmain.js
@@ -1,5 +1,8 @@
-//var extId = 'ioaancmfccbkoknfeibefmdahkpiincg';
-var extId = 'ioaancmfccbkoknfeibefmdahkpiinch';
+var extId = 'ioaancmfccbkoknfeibefmdahkpiincg';
+//var extId = 'ioaancmfccbkoknfeibefmdahkpiinch';
 chrome.runtime.sendMessage(extId, {installCheck: true}, (res) => {
+    // インストールされていない場合は
+    // res=undefined
+    // になる
     console.log(res);
 });
