879746b886d16d3b7f48841b4d7e1c1cf5ad9299
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-08-25 18:29:35 +0900

    commit

--- extension/cs.js
+++ extension/cs.js
@@ -58,7 +58,7 @@ function stepDialogShow(stepNo) {
             classAdd(btnModeChange, 'opdesc-mode');
         } else if(stepNo === 9) {
             classRemove(btnModeChange, 'opdesc-mode');
-            accountId = accounts[0];
+            accountId = objKeys(debugAccounts)[0];
             btnModeChange.click();
         }
     }
