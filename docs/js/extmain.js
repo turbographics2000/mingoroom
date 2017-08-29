0b26b9aa94aa523900c3e26cd7d0cd17231a7fbf
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-08-29 22:19:13 +0900

    commit

--- docs/js/extmain.js
+++ docs/js/extmain.js
@@ -41,6 +41,7 @@ chrome.runtime.sendMessage(extId, { installCheck: true }, res => {
         localStorage.setItem('step', step);
     } else {
         if (step === 'extNotInstalled') {
+            tutorialMask.style.background = 'gray';
             tutorialMask.classList.remove('hide');
             welcomDialog.classList.remove('hide');
         } else if (step === 'installButtonClicked') {
