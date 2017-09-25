e6c2eec6976e1b15ef7416a0f3be661c2aa4bb9e
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-09-25 17:00:04 +0900

    commit

--- docs/ext/cs.js
+++ docs/ext/cs.js
@@ -621,7 +621,7 @@ function roomDialogShow(isView) {
     var comment = currentRoomData.comment || '';
     var owner = currentRoomData.owner;
 
-    roomViewOwnerAvatar.src = accounts[onwer.twitterScrName].avatar;
+    roomViewOwnerAvatar.src = accounts[onwer].avatar;
     roomViewSummary.textContent = [
         fmt('y/m/d h:m', year, month, day, hour, minute),
         courses[course].short,
